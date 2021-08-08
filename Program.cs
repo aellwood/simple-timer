19074339a94c706d156e3adef6ad4af74707ec30
Andrew Ellwood <andrew.e.ellwood@gmail.com>
2021-08-08 17:41:21 +0100

    add program

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace timer
+{
+    class Program
+    {
+        private const string fileName = "timings.txt";
+
+        static void Main(string[] args)
+        {
+            if (File.Exists(fileName)) 
+            {
+                Console.WriteLine($"{fileName} already exists");
+            }
+            else 
+            {
+                using (StreamWriter sw = File.CreateText(fileName))
+                {
+                    Log(sw);
+                }	
+
+                while(true) 
+                {
+                    Thread.Sleep(60000);
+
+                    using (StreamWriter sw = File.AppendText(fileName))
+                    {
+                        Log(sw);
+                    }
+                }
+            }
+        }
+
+        private static void Log(StreamWriter sw) 
+        {
+            sw.WriteLine(TimeOnly.FromDateTime(DateTime.UtcNow));
+        }
+    }
+}
